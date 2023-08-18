7c08b8402a537200601f7660e61cce1495b1ea56
ahmadtp1 <streameren1@gmail.com>
2023-08-18 15:43:09 +0530

    Crud Barang

--- resources/views/barang/create.blade.php
+++ resources/views/barang/create.blade.php
@@ -1,72 +1,64 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>TAMBAH BARANG</title>
-  </head>
-  <body>
-
-    <div class="container mt-5">
-        <form action="{{ route("barang.store") }}" method="post">
-            @csrf
-            @method('post')
-            <div class="mb-3">
-              <label class="form-label">Mobil</label>
-              <input value="{{ old('plat_mobil')}}" name="plat_mobil" type="text" class="form-control @error('plat_mobil') is-invalid @enderror">
-                @error('plat_mobil')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Jenis Mobil</label>
-                <input value="{{ old('jenis_kendaraan')}}" name="jenis_kendaraan" type="text" class="form-control @error('jenis_kendaraan') is-invalid @enderror">
-                  @error('jenis_kendaraan')
-                      <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-              </div>
-              <div class="mb-3">
-                <label class="form-label">Merek Mobil</label>
-                <input value="{{ old('merek')}}" name="merek" type="text" class="form-control @error('merek') is-invalid @enderror">
-                  @error('merek')
-                      <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-              </div>
-              <div class="mb-3">
-                <label class="form-label">Model Mobil</label>
-                <input value="{{ old('model')}}" name="model" type="text" class="form-control @error('model') is-invalid @enderror">
-                  @error('model')
-                      <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-              </div>
-              <div class="mb-3">
-                <label class="form-label">Warna Mobil</label>
-                <input value="{{ old('warna')}}" name="warna" type="text" class="form-control @error('warna') is-invalid @enderror">
-                  @error('warna')
-                      <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-              </div>
-              <div class="mb-3">
-                <label class="form-label">Kapasitas Penumpang</label>
-                <input value="{{ old('kapasitas_penumpang')}}" name="kapasitas_penumpang" type="text" class="form-control @error('kapasitas_penumpang') is-invalid @enderror">
-                  @error('kapasitas_penumpang')
-                      <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-              </div>
-              <div class="mb-3">
-                <label class="form-label">Harga Sewa</label>
-                <input value="{{ old('harga_sewa')}}" name="harga_sewa" type="text" class="form-control @error('harga_sewa') is-invalid @enderror">
-                  @error('harga_sewa')
-                      <div class="invalid-feedback">{{ $message }}</div>
-                  @enderror
-              </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-  </body>
-</html>
-
+@extends('components.main')
+<div class="container mt-5">
+    <form action="{{ route('barang.store') }}" method="post">
+        @csrf
+        @method('post')
+        <div class="mb-3">
+            <label class="form-label">Mobil</label>
+            <input value="{{ old('mobile') }}" name="mobile" type="text"
+                class="form-control @error('mobile') is-invalid @enderror">
+            @error('mobile')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Jenis Mobil</label>
+            <input value="{{ old('vehicle_type') }}" name="vehicle_type" type="text"
+                class="form-control @error('vehicle_type') is-invalid @enderror">
+            @error('vehicle_type')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Merek Mobil</label>
+            <input value="{{ old('brand') }}" name="brand" type="text"
+                class="form-control @error('brand') is-invalid @enderror">
+            @error('brand')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Model Mobil</label>
+            <input value="{{ old('model') }}" name="model" type="text"
+                class="form-control @error('model') is-invalid @enderror">
+            @error('model')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Warna Mobil</label>
+            <input value="{{ old('color') }}" name="color" type="text"
+                class="form-control @error('color') is-invalid @enderror">
+            @error('color')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Kapasitas Penumpang</label>
+            <input value="{{ old('passenger_capacity') }}" name="passenger_capacity" type="number"
+                class="form-control @error('passenger_capacity') is-invalid @enderror">
+            @error('passenger_capacity')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Harga Sewa</label>
+            <input value="{{ old('rental_price') }}" name="rental_price" type="text"
+                class="form-control @error('rental_price') is-invalid @enderror">
+            @error('rental_price')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -1,54 +1,47 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('components.main')
+<div class="container mt-5">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <a href="{{ route('barang.create') }}" class="btn btn-primary">Tambah Barang</a>
 
-    <title>TABEL BARANG</title>
-  </head>
-  <body>
-
-    <div class="container mt-5">
-
-        <a href="{{ route('barang .create')}}" class="btn btn-primary">Tambah Barang</a>
-
-        @if ($pesan = session('berhasil'))
+    @if ($pesan = session('berhasil'))
         <div class="alert alert-primary" role="alert">
-           {{ $pesan }}
+            {{ $pesan }}
         </div>
-        @endif
-<table class="table">
-    <thead>
-      <tr>
-        <th scope="col">No</th>
-        <th scope="col">Mobil</th>
-        <th scope="col">Jenis Mobil</th>
-        <th scope="col">Merk Mobil</th>
-        <th scope="col">Model Mobil</th>
-        <th scope="col">Warna Mobil</th>
-        <th scope="col">Kapasitas Penumpang</th>
-        <th scope="col">Harga Sewa</th>
-      </tr>
-    </thead>
-    <tbody>
-        @foreach ($barang as $index => $brng)
-        <tr>
-            <th scope="row">{{ $index}}</th>
-                <td>{{ $brng->plat_mobil }}</td>
-                <td>{{ $brng->jenis_kendaraan }}</td>
-                <td>{{ $brng->merek }}</td>
-                <td>{{ $brng->model }}</td>
-                <td>{{ $brng->warna }}</td>
-                <td>{{ $brng->kapasitas_penumpang }}</td>
-                <td>{{ $brng->harga_sewa }}</td>
-                <td>
-                    <a href="#" class="btn btn-primary">Edit</a>
-                </td>
-        </tr>
-        @endforeach
-    </tbody>
-  </table>
+    @endif
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Mobil</th>
+                <th scope="col">Jenis Mobil</th>
+                <th scope="col">Merk Mobil</th>
+                <th scope="col">Model Mobil</th>
+                <th scope="col">Warna Mobil</th>
+                <th scope="col">Kapasitas Penumpang</th>
+                <th scope="col">Harga Sewa</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($barang as $index => $brng)
+                <tr>
+                    <th scope="row">{{ $index + 1 }}</th>
+                    <td>{{ $brng->mobile }}</td>
+                    <td>{{ $brng->vehicle_type }}</td>
+                    <td>{{ $brng->brang }}</td>
+                    <td>{{ $brng->model }}</td>
+                    <td>{{ $brng->color }}</td>
+                    <td>{{ $brng->passenger_capacity }}</td>
+                    <td>{{ $brng->rental_price }}</td>
+                    <td>
+                        <form action="{{ route('barang.destroy', $brng->id) }}" method="POST">
+                            @csrf
+                            @method('delete')
+                            <a href="{{ route('barang.edit', $brng->id) }}" class="btn btn-primary btn-sm">Edit</a>
+                            <button class="btn btn-danger btn-sm">HAPUS</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
