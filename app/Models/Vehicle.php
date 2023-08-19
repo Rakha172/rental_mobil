5d79d2adcfc37b7944426359260b8c30d9053bd4
unknown <rhmakhrani@gmail.com>
2023-08-19 17:29:50 +0700

    ngubah models

---
+++ app/Models/Vehicle.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vehicle extends Model
+{
+    protected $table = 'vehicle';
+    protected $fillabel = [
+        'image',
+        'vehicle_type',
+        'brand',
+        'color',
+        'passenger_capacity',
+        'status',
+        'created_at',
+        'updated_at'
+    ];
+}
