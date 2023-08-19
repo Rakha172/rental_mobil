8612ba7770d6bd1b68689744495b2d0b8aa8ffed
unknown <rhmakhrani@gmail.com>
2023-08-19 17:28:29 +0700

    ngubah controller

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vehicle;
+
+use Illuminate\Http\Request;
+
+class VehicleController extends Controller
+{
+    public function index()
+    {
+        $vehicle = Vehicle::all();
+
+        return view('vehicle.index', ['vehicle' => $vehicle]);
+    }
+
+    public function create()
+    {
+        return view('vehicle.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'image' => 'required',
+            'vehicle_type' => 'required',
+            'brand' => 'required',
+            'color' => 'required',
+            'pasengger_capacity' => 'required',
+            'status' => 'required',
+        ]);
+
+        $vehicle = new Vehicle;
+        $vehicle->image = $request->image;
+        $vehicle->vehicle_type = $request->vehicle_type;
+        $vehicle->brand = $request->brand;
+        $vehicle->color = $request->color;
+        $vehicle->pasengger_capacity = $request->pasengger_capacity;
+        $vehicle->status = $request->status;
+        $vehicle->save();
+
+        return to_route('barang.index')->with('succes', 'data ditambah');
+    }
+
+    public function edit($id)
+    {
+        $vehicle = Vehicle::find($id);
+        if (!$vehicle)
+            return redirect()->route('barang.index')
+                ->with('error_message', 'pembeli dengan id = ' . $id . ' tidak ditemukan');
+        return view('barang.edit', [
+            'vehicle' => $vehicle,
+        ]);
+    }
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'image' => 'required',
+            'vehicle_type' => 'required',
+            'brand' => 'required',
+            'color' => 'required',
+            'pasengger_capacity' => 'required',
+            'status' => 'required',
+        ]);
+
+        $vehicle = Vehicle::find($id);
+        $vehicle->image = $request->image;
+        $vehicle->vehicle_type = $request->vehicle_type;
+        $vehicle->brand = $request->brand;
+        $vehicle->color = $request->color;
+        $vehicle->pasengger_capacity = $request->pasengger_capacity;
+        $vehicle->status = $request->status;
+        $vehicle->save();
+
+        return to_route('barang.index')->with('succes', 'data ditambah');
+    }
+    public function destroy($id)
+    {
+        $vehicle = Vehicle::find($id);
+        $vehicle->delete();
+
+        return back()->with('succes', 'data dihapus');
+    }
+
+
+}
