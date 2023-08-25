dbf86dfdfcce09b4556e516a24dd4353ae5f97f2
yusup <ymaolana06@gmail.com>
2023-08-25 11:20:49 +0700

    edit register view

--- resources/views/login/register.blade.php
+++ resources/views/login/register.blade.php
@@ -7,97 +7,95 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-</head>
-<style>
-    .container {
-        width: 400px;
-    }
-</style>
 
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
+
+</head>
 <body>
-    <div class="container mt-5 width">
-        <div class="card">
-            <div class="body-card">
-                <form action="{{ route('register.store') }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <h1 class="text-center">Sign Up</h1>
-                    <div class="form-floating mb-2">
-                        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
-                            id="name" value="{{ old('name') }}">
-                        @error('name')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                        <label for="name">Full Name</label>
-                    </div>
-                    <div class="form-floating mb-2">
-                        <select name="gender" id="gender" class="form-control">
-                            <option value="">--Select Gender--</option>
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                        </select>
-                        <label for="gender">Gender</label>
-                    </div>
-                    <div class="form-floating mb-2">
-                        <input type="number" name="phone_number"
-                            class="form-control @error('phone_number') is-invalid @enderror" id="phone_number"
-                            value="{{ old('phone_number') }}">
-                        @error('phone_number')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                        <label for="phone_number">Phone</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="text" name="address" class="form-control @error('address') is-invalid @enderror"
-                            id="address" value="{{ old('address') }}">
-                        @error('address')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                        <label for="address">Address</label>
-                    </div>
-                    <div class="form-floating mb-2">
-                        <input type="file" name="id_card_photo"
-                            class="form-control @error('id_card_photo') is-invalid @enderror" id="id_card_photo"
-                            value="{{ old('id_card_photo') }}" accept="image/*"
-                            onchange="document.getElementById('output').src = window.URL.createObjectURL(this.files[0])">
-                        @error('id_card_photo')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                        <label for="id_card_photo">Id Card</label>
-                        <div class="mt-3"><img src="" id="output" width="200"></div>
-                    </div>
-                    <div class="form-floating mb-2">
-                        <input type="file" name="driver_licence_photo"
-                            class="form-control @error('driver_licence_photo') is-invalid @enderror"
-                            id="driver_licence_photo" value="{{ old('driver_licence_photo') }}"accept="image/*"
-                            onchange="document.getElementById('output2').src = window.URL.createObjectURL(this.files[0])">
-                        @error('driver_licence_photo')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                        <label for="driver_licence_photo">Driver Licence</label>
-                        <div class="mt-3"><img src="" id="output2" width="200"></div>
-                    </div>
-                    <div class="form-floating mb-2">
-                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
-                            id="email" value="{{ old('email') }}">
-                        @error('email')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                        <label for="email">Your Email</label>
-                    </div>
-                    <div class="form-floating mb-2">
-                        <input type="password" name="password"
-                            class="form-control @error('password') is-invalid @enderror" id="password"
-                            value="{{ old('password') }}">
-                        @error('password')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                        <label for="password">Your Password</label>
-                    </div>
-                    <button class="btn btn-primary">Submit</button>
-                    <a href="{{ route('login.index') }}">have a account?</a>
-                </form>
+    <div class="container-fluid">
+        <form action="{{ route('register.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <h1>Sign Up</h1>
+            <div class="form-floating">
+                <input  style="background-color:  rgb(155, 152, 152);" type="text" name="name" placeholder="Full Name" class="form-control @error('name') is-invalid @enderror"
+                    id="name" value="{{ old('name') }}">
+                @error('name')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+                <label for="name">Full Name</label>
             </div>
-        </div>
+            <div class="form-floating">
+                <input  style="background-color:  rgb(155, 152, 152);" type="number" placeholder="Phone" name="phone_number"
+                    class="form-control @error('phone_number') is-invalid @enderror" id="phone_number"
+                    value="{{ old('phone_number') }}">
+                @error('phone_number')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+                <label for="phone_number">Phone</label>
+            </div>
+            <div class="form-floating">
+                <input style="background-color:  rgb(155, 152, 152);" type="text" placeholder="Address" name="address" class="form-control @error('address') is-invalid @enderror"
+                    id="address" value="{{ old('address') }}">
+                @error('address')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+                <label for="address">Address</label>
+            </div>
+            <div class="gender">
+                <label for="exampleInputEmail1" class="form-label">Gender  :</label>
+                <input type="radio" name="Gender" value="Male">Male
+                <input type="radio" name="Gender" value="Female">Female
+            </div>
+            <div class="label">
+                <label for="id_card_photo">Id Card Photo</label>
+            </div>
+            <div class="form-floating">
+                <input type="image" name="id_card_photo"
+                    @error('id_card_photo') is-invalid @enderror id="id_card_photo"
+                    value="{{ old('id_card_photo') }}" accept="image/*"
+                    onchange="document.getElementById('output').src = window.URL.createObjectURL(this.files[0])">
+                @error('id_card_photo')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+                <img src="" id="output" width="250" height="100">
+            </div>
+            <div class="label">
+                <label for="driver_licence_photo">Driver Licence Photo</label>
+            </div>
+            <div class="form-floating">
+                <input type="image" name="driver_licence_photo"
+                    @error('driver_licence_photo') is-invalid @enderror
+                    id="driver_licence_photo" value="{{ old('driver_licence_photo') }}"accept="image/*"
+                    onchange="document.getElementById('output2').src = window.URL.createObjectURL(this.files[0])">
+                @error('driver_licence_photo')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+                <img src="" id="output2" width="250" height="100">
+            </div>
+            <div class="form-floating">
+                <input style="background-color:  rgb(155, 152, 152);" type="email" placeholder="Email" name="email" class="form-control @error('email') is-invalid @enderror"
+                    id="email" value="{{ old('email') }}">
+                @error('email')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+                <label for="email">Your Email</label>
+            </div>
+            <div class="form-floating">
+                <input style="background-color:  rgb(155, 152, 152);" type="password" placeholder="Password" name="password"
+                    class="form-control @error('password') is-invalid @enderror" id="password"
+                    value="{{ old('password') }}">
+                @error('password')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+                <label for="password">Your Password</label>
+            </div>
+            <div class="text-center">
+                <button>Submit</button>
+            </div>
+            <div class="text-center ">
+                <a href="{{ route('login.index') }}">have a account?</a>
+            </div>
+        </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
