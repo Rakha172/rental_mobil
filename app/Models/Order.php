a7267ae0ea2a86ea151f54d773ba4b8dc1264977
unknown <rhmakhrani@gmail.com>
2023-08-30 14:44:24 +0700

    models order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,5 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    use HasFactory;
+    protected $table = 'order';
+    protected $fillabel = [
+        'rental_date',
+        'return_date',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $guarded = [];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function vehicle_packages()
+    {
+        return $this->belongsTo(Vehicle_Package::class);
+    }
 }
