85195f92298ff7f3acaa890eefebb679812a9ab2
unknown <rhmakhrani@gmail.com>
2023-08-31 13:57:47 +0700

    ngubah tipe price

---
+++ resources/views/order/create.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Tambah Order</title>
+  </head>
+  <body>
+
+    <div class="container mt-5">
+        <form action="{{ route("order.store") }}" method="post">
+            @csrf
+            @method('post')
+                <div class="mb-3">
+                    <label class="form-label">User</label>
+                    <select name="user_id" class="form-control @error('user_id') is-invalid @enderror">
+                        <option value="">Pilih</option>
+                        @foreach ($users as $users)
+                            <option @selected(old('users') == $users->id) value={{ $users->id }}>{{ $users->name }}</option>
+                        @endforeach
+                    </select>
+                    @error('user_id')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                  </div>
+                  <div class="mb-3">
+                    <label class="form-label">Vehicle Package</label>
+                    <select name="vehicle_package_id" class="form-control @error('vehicle_package_id') is-invalid @enderror">
+                        <option value="">Pilih</option>
+                        @foreach ($vehicle_packages as $vehicle_packages)
+                            <option @selected(old('vehicle_packages') == $vehicle_packages->id) value={{ $vehicle_packages->id }}>{{ $vehicle_packages->package_name }}</option>
+                        @endforeach
+                    </select>
+                    @error('vehicle_package_id')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                  </div>
+                  <div class="mb-3">
+                    <label class="form-label">Rental Date</label>
+                    <input value="{{ old('rental_date')}}" name="rental_date" type="date" class="form-control @error('rental_date') is-invalid @enderror">
+                    @error('rental_date')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Return date</label>
+                    <input value="{{ old('return_date')}}" name="return_date" type="date" class="form-control @error('return_date') is-invalid @enderror">
+                  @error('return_date')
+                      <div class="invalid-feedback">{{ $message }}</div>
+                  @enderror
+              </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
+
---
+++ resources/views/order/index.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Order</title>
+  </head>
+  <body>
+
+    <div class="container mt-5">
+
+        <a href="{{ route('order.create')}}" class="btn btn-primary">Tambah</a>
+
+        @if ($pesan = session('berhasil'))
+        <div class="alert alert-primary" role="alert">
+           {{ $pesan }}
+        </div>
+        @endif
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">NO</th>
+                <th scope="col">Name</th>
+                <th scope="col">Package Name</th>
+                <th scope="col">Rental Date</th>
+                <th scope="col">Return Date</th>
+                <th scope="col">Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($order as $index => $ordr)
+              <tr>
+                <th scope="row">{{ $index}}</th>
+                <td>{{ $ordr->users ? $ordr->users->name : 'Name Not Found' }}</td>
+                <td>{{ $ordr->vehicle_packages ? $ordr->vehicle_packages->package_name : 'Package Name Not Found' }}</td>
+                <td>{{ $ordr->rental_date }}</td>
+                <td>{{ $ordr->return_date }}</td>
+                <td>
+                    <a href="{{ route('order.edit', $ordr->id) }}" class="btn btn-primary">Edit</a>
+
+                    <form action="{{ route('order.destroy', $ordr->id)}}"method="POST">
+                        @csrf
+                        @method('delete')
+
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+    </div>
+    <td colspan="n">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
