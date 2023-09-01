540255a74598d2c9a2787c377b95758e66202b00
ahmadtp1 <streameren1@gmail.com>
2023-09-01 16:05:20 +0530

    phone number biar ga error, select gender :P

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,7 +34,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'gender' => 'required',
-            'phone_number' => 'required',
+            'phone_number' => ['required', 'string', 'max:255', 'unique:'.User::class],
             'address' => 'required',
             'id_card_photo' => 'required|image|mimes:png,jpg|max:2040',
             'driver_licence_photo' => 'required|image|mimes:png,jpg|max:2040',
