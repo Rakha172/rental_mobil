eb147795f595176a8543cad7203c7a48106358d6
unknown <rhmakhrani@gmail.com>
2023-09-04 09:56:21 +0700

    order index

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -21,35 +21,6 @@
               </tr>
             </thead>
             <tbody>
-                @foreach ($order as $index => $ordr)
-              <tr>
-                <th scope="row">{{ $index}}</th>
-                <td>{{ $ordr->users ? $ordr->users->name : 'Name Not Found' }}</td>
-                <td>{{ $ordr->vehicle_package ? $ordr->vehicle_package->package_name : 'Package Name Not Found' }}</td>
-                <td>{{ $ordr->rental_date }}</td>
-                <td>{{ $ordr->return_date }}</td>
-                <td>
-                    <a href="{{ route('order.edit', $ordr->id) }}" class="btn btn-primary">Edit</a>
-                    <form action="{{ route('order.destroy', $ordr->id)}}"method="POST">
-                        @csrf
-                        @method('delete')
-
-                        <button type="submit" class="btn btn-danger">Delete</button>
-                    </form>
-                </td>
-              </tr>
-              @endforeach
-            </tbody>
-          </table>
-    </div>
-    <td colspan="n">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-  </body>
-</html>
-            </tr>
-        </thead>
-        <tbody>
             @foreach ($order as $index => $ordr)
                 <tr>
                     <th scope="row">{{ $index }}</th>
@@ -60,7 +31,6 @@
                     <td>{{ $ordr->return_date }}</td>
                     <td>
                         <a href="{{ route('order.edit', $ordr->id) }}" class="btn btn-primary">Edit</a>
-
                         <form action="{{ route('order.destroy', $ordr->id) }}"method="POST">
                             @csrf
                             @method('delete')
