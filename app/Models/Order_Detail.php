915306e6012f04ccb544835d2e50ebadbeee0f06
unknown <rhmakhrani@gmail.com>
2023-09-06 10:32:06 +0700

    models order_detail

---
+++ app/Models/Order_Detail.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order_Detail extends Model
+{
+    protected $table = 'order_detail';
+
+    protected $fillabel = [
+        'vehicle_status',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $guarded = [];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
