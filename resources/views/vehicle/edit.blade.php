05af27e7bf98522db26aad512ce7e8fa7f1a21fb
yusup <ymaolana06@gmail.com>
2023-09-06 09:03:55 +0700

    edit vehicle

--- resources/views/vehicle/edit.blade.php
+++ resources/views/vehicle/edit.blade.php
@@ -5,23 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/stylevehicleedit.css">
     <title>Edit Vehicle</title>
-    {{-- dhgdrrg --}}
-    {{-- ygfcyctcy --}}
   </head>
   <body>
-    <div class="container mt-5">
+    <div class="container-fluid">
         <form action="{{ route("vehicle.update", $vehicle->id) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('put')
-            <div class="form-floating mb-2">
-                <input type="file" name="image">
-                <img src="{{ asset('images')}}" alt="">
-                <div class="mt-3"><img src="" id="output" width="200"></div>
-            </div>
-            <div class="mb-3">
+            <h1>Edit Vehicle</h1>
+        <form action="{{ route("vehicle.update", $vehicle->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('put')
+            <div class="form-floating">
+                <div class="">
+                    <input type="file" name="image">
+                    <img src="{{ asset($vehicle->image) }}" alt="{{ $vehicle->image }}" width="150">
+                    <img src="{{ asset('upload/vehicle/' .  $vehicle->image)}}" alt="">
+
+                </div>
+                @error('image')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
                 <label class="form-label">Vehicle Type</label>
-                <select name="vehicle_type" class="form-control @error('vehicle_type') is-invalid @enderror">
+            <div class="form-floating">
+                <select style="background-color:rgb(167, 166, 166)" name="vehicle_type" class="form-control @error('vehicle_type') is-invalid @enderror">
                     <option value="">choose</option>
                     <option @selected(old('vehicle_type', $vehicle->vehicle_type) == 'Sedan') value="Sedan">Sedan</option>
                     <option @selected(old('vehicle_type', $vehicle->vehicle_type) == 'SUV') value="SUV">SUV</option>
@@ -32,9 +41,9 @@
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
               </div>
-              <div class="mb-3">
-                <label class="form-label">Mobile Brand</label>
-                <select name="brand" class="form-control @error('brand') is-invalid @enderror">
+              <label class="form-label">Mobile Brand</label>
+              <div class="form-floating">
+                <select style="background-color:rgb(167, 166, 166)" name="brand" class="form-control @error('brand') is-invalid @enderror">
                     <option value="">choose</option>
                     <option @selected(old('brand', $vehicle->brand) == 'Toyota') value="Toyota">Toyota</option>
                     <option @selected(old('brand', $vehicle->brand) == 'Honda') value="Honda">Honda</option>
@@ -48,9 +57,9 @@
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
               </div>
-              <div class="mb-3">
-                <label class="form-label">Mobile Color</label>
-                <select name="color" class="form-control @error('color') is-invalid @enderror">
+              <label class="form-label">Mobile Color</label>
+              <div class="form-floating">
+                <select style="background-color:rgb(167, 166, 166)" name="color" class="form-control @error('color') is-invalid @enderror">
                     <option value="">choose</option>
                     <option @selected(old('color', $vehicle->color) == 'Black') value="Black">Black</option>
                     <option @selected(old('color', $vehicle->color) == 'White') value="White">White</option>
@@ -60,9 +69,9 @@
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
               </div>
-              <div class="mb-3">
-                <label class="form-label">Passenger Capacity</label>
-                <select name="passenger_capacity" class="form-control @error('passenger_capacity') is-invalid @enderror">
+              <label class="form-label">Passenger Capacity</label>
+              <div class="form-floating">
+                <select style="background-color:rgb(167, 166, 166)" name="passenger_capacity" class="form-control @error('passenger_capacity') is-invalid @enderror">
                     <option value="">choose</option>
                     <option @selected(old('passenger_capacity', $vehicle->passenger_capacity) == '15 passenger') value="15 Passenger">15 Passenger</option>
                     <option @selected(old('passenger_capacity', $vehicle->passenger_capacity) == '7 passenger') value="7 Passenger">7 Passenger</option>
@@ -81,6 +90,11 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
+    <script>
+        function loadImage(event) {
+            var output = document.getElementById('editedImage');
+            output.src = URL.createObjectURL(event.target.files[0]);
+        }
+    </script>
   </body>
 </html>
