015f285dda9787de6fb90c66e88afd0343f5194b
dhafaprojek <dafaassidik9a@gmail.com>
2023-09-09 11:09:49 +0700

    tampilan vehicle_package

---
+++ resources/views/paket/index.blade.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('/css/homepage.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+    <header>
+        <a href="" class="logo"><img src="{{asset("images/jeep.png")}}" alt=""></a>
+
+       <div class="bx bx-menu" id="menu-icon"></div>
+       <ul class="navbar">
+
+        <li><a href="#">Vehicle</a></li>
+        <li><a href="#">Vehicle Package</a></li>
+        <li><a href="#">Booking</a></li>
+        <li><a href="#">Order</a></li>
+
+       </ul>
+       <div class="header-btn">
+        <a href="#" class="sign-up"></a>
+        <a href="#" class="sign-in">Profil</a>
+
+       </div>
+    </header>
+    <!-- Vehicle -->
+    <section class="package" id="package">
+        <div class="heading">
+            <br>
+            <br>
+            <h1>Vehicle Package</h1>
+        </div>
+        <div class="package-container">
+            <div class="card">
+                <img src="{{asset()}}" alt="Avatar" width="300px" height="300px">
+
+                <div class="container">
+
+                  <h3><b>Name Car</b></h3>
+                    <h4><b>Paket</b></h4>
+                   <form action="">
+                    <select class="select">
+                      <option >pilih</option>
+                      <option value="volvo">3 Hari || Rp.300.000</option>
+                      <option value="saab">Saab</option>
+                      <option value="opel">Opel</option>
+                      <option value="audi">Audi</option>
+                    </select>
+                   <br>
+                  </form>
+                  <br>
+                  <a href="#" class="btn">booking</a>
+
+                </div>
+              </div>
+
+              <div class="card">
+                <img src="{{asset()}}" alt="Avatar" width="200px" height="300px">
+
+                 <div class="container">
+
+                  <h3><b>Name Car</b></h3>
+                   <h4><b>Paket</b></h4>
+                   <form action="">
+                    <select class="select">
+                      <option >pilih</option>
+                      <option value="volvo">3 Hari || Rp.300.000</option>
+                      <option value="saab">Saab</option>
+                      <option value="opel">Opel</option>
+                      <option value="audi">Audi</option>
+                    </select>
+                   <br>
+                   <br>
+                   <a href="#" class="btn">booking</a>
+                  </form>
+                </div>
+              </div>
+
+              <div class="card">
+                <img src="{{asset()}}" alt="Avatar" width="300px" height="300px">
+
+                <div class="container">
+
+                  <h3><b>Name Car</b></h3>
+                   <h4><b>Paket</b></h4>
+                   <form action="">
+                    <select class="select">
+                      <option >pilih</option>
+                      <option value="volvo">3 Hari || Rp.300.000</option>
+                      <option value="saab">Saab</option>
+                      <option value="opel">Opel</option>
+                      <option value="audi">Audi</option>
+                    </select>
+                   <br>
+                   <br>
+                   <a href="#" class="btn">booking</a>
+                  </form>
+                </div>
+              </div>
+
+              <div class="card">
+                <img src="{{asset()}}" alt="Avatar" width="300px" height="300px">
+
+                <div class="container">
+
+                  <h3><b>Name Car</b></h3>
+                   <h4><b>Paket</b></h4>
+                   <form action="">
+                    <select class="select">
+                      <option value="">pilih</option>
+                      <option value="volvo">3 Hari || Rp.300.000</option>
+                      <option value="saab">Saab</option>
+                      <option value="opel">Opel</option>
+                      <option value="audi">Audi</option>
+                    </select>
+                   <br>
+                   <br>
+                   <a href="#" class="btn">booking</a>
+                  </form>
+                </div>
+              </div>
+
+              <div class="card">
+                <img src="{{asset()}}" alt="Avatar" width="300px" height="300px">
+
+                <div class="container">
+
+                  <h3><b>Name Car</b></h3>
+                   <h4><b>Paket</b></h4>
+                   <form action="">
+                    <select class="select">
+                      <option value="">pilih</option>
+                      <option value="volvo">3 Hari || Rp.300.000</option>
+                      <option value="saab">Saab</option>
+                      <option value="opel">Opel</option>
+                      <option value="audi">Audi</option>
+                    </select>
+                   <br>
+                   <br>
+                   <a href="#" class="btn">booking</a>
+                  </form>
+                </div>
+              </div>
+
+              <div class="card">
+                <img src="{{asset()}}" alt="Avatar" width="300px" height="300px">
+
+                <div class="container">
+
+                  <h3><b>Name Car</b></h3>
+                   <h4><b>Paket</b></h4>
+                   <form action="">
+                    <select class="select">
+                      <option value="">pilih</option>
+                      <option value="volvo">3 Hari || Rp.300.000</option>
+                      <option value="saab">Saab</option>
+                      <option value="opel">Opel</option>
+                      <option value="audi">Audi</option>
+                    </select>
+                   <br>
+                   <br>
+                   <a href="#" class="btn">booking</a>
+                  </form>
+                </div>
+              </div>
+        </div>
+    </section>
+
+    <script src="main.js"></script>
+</body>
+</html>
