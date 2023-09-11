79602490eeb24002eb6611ff6a2e4ae697536f2c
unknown <rhmakhrani@gmail.com>
2023-09-11 17:34:37 +0700

    model

---
+++ app/Models/Payment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+   protected $table = 'payment';
+
+   protected $fillabel = [
+    'total_price',
+    'payment_method',
+    'proof_of_transaction'
+   ];
+
+   public function Order()
+   {
+    return $this->belongsTo(Order::class);
+   }
+}
