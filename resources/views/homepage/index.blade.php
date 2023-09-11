3b926f6ca71cd72dd76eb4fadf450bc203acb898
yusup <ymaolana06@gmail.com>
2023-09-11 13:19:24 +0700

    homepage

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -41,178 +41,30 @@
         </form>
        </div>
     </header>
-    <!-- Vehicle -->
-yusup
-  <!-- Vehicle -->
-@if(isset($vehicle))
-<section class="services" id="services">
-    <div class="services-container">
-        @foreach ( $vehicle as $vhcl )
-        <div class="box">
-            <div class="box-img">
-                <img src="{{ asset($vhcl->image)}}" width="100px" height="100px">
-
-            </div>
-            <ul>
-                <li>
-                    {{ $vhcl->brand }}
-                </li>
-            </ul>
-                <ul>
-                    <li>
-                        {{ $vhcl->vehicle_type }}
-                    </li>
-                </ul>
-            <a href="#" class="btn">booking</a>
-        </div>
-        @endforeach
-    </div>
-</section>
-@endif
-
-
-            {{-- <div class="box">
-=======
-    <section class="services" id="services">
-        <div class="heading">
-            <br>
-            <br>
-            <h1>Vehicle</h1>
-        </div>
-        <div class="services-container">
-            @if ($vehicle->count()>0)
-            @foreach ($vehicle as $index => $vhcle)
-            <div class="box">
-                <div class="box-img">
-                    <img src="{{ asset($vhcle->image)}}">
-                </div>
-                    <p>{{ $index+1}}</p>
-                    <h3>Name Car</h3>
-                    <h3>Brand : {{$vhcle->brand}}</h3>
-                    <h3>Color :  {{$vhcle->color}}</h3>
-                    <h3>Passenger capacity : {{$vhcle->passenger_capacity}}</h3>
-                    <h2>Harga /1<span>Day</span></h2>
-                    <a href="" class="btn">booking</a>
-            </div>
-            @endforeach
-            @elseif (Auth::user()->role == 'admin')
-                <p>Tidak ada data vehicle, silahkan mengisi-> data di vehicle </p>
-                <a href="{{ route('vehicle.index') }}">Vehicle</a>
-            @endif
-            <div class="box">
- main
-                <div class="box-img">
-                    <img src="{{asset('images/')}}" alt="">
-                </div>
-                    <p>2023</p>
-                    <h3>Name Car</h3>
-                    <h2>Harga /<span>1 Day</span></h2>
+        @if(isset($vehicle))
+        <section class="services" id="services">
+            <div class="services-container">
+                @foreach ( $vehicle as $vhcl )
+                <div class="box">
+                    <div class="box-img">
+                        <img src="{{ asset($vhcl->image)}}">
+                    </div>
+                    <ul>
+                        <li>
+                            {{ $vhcl->brand }}
+                        </li>
+                    </ul>
+                        <ul>
+                            <li>
+                                {{ $vhcl->vehicle_type }}
+                            </li>
+                        </ul>
                     <a href="#" class="btn">booking</a>
-            </div>
-
-            <div class="box">
-                <div class="box-img">
-                    <img src="{{asset('images/')}}" alt="">
                 </div>
-                    <p>2023</p>
-                    <h3>Name Car</h3>
-                    <h2>Harga/<span>Day</span></h2>
-                    <a href="#" class="btn">booking</a>
+                @endforeach
             </div>
-
-            <div class="box">
-                <div class="box-img">
-                    <img src="{{asset('images/')}}" alt="">
-                </div>
-                    <p>2023</p>
-                    <h3>Name Car</h3>
-                    <h2>Harga /<span>Day</span></h2>
-                    <a href="#" class="btn">booking</a>
-            </div>
-
-             <div class="box">
-                <div class="box-img">
-                    <img src="{{asset('images/')}}" alt="">
-                </div>
-                    <p>2023</p>
-                    <h3>Name Car</h3>
-                    <h2>Harga /<span>Day</span></h2>
-                    <a href="#" class="btn">booking</a>
-            </div>
-
-            <div class="box">
-                <div class="box-img">
-                    <img src="{{asset('images/')}}" alt="">
-                </div>
-                    <p>2023</p>
-                    <h3>Name Car</h3>
-                    <h2>Harga/ <span>Day</span></h2>
-                    <a href="#" class="btn">booking</a>
-            </div>
-
-            <div class="box">
-                <div class="box-img">
-                    <img src="{{asset('images/')}}" alt="">
-                </div>
-                    <p>2023</p>
-                    <h3>Name Car</h3>
-                    <h2>Harga/ <span>Day</span></h2>
-                    <a href="#" class="btn">booking</a>
-            </div>
-            <div class="box">
-                <div class="box-img">
-                    <img src="{{asset('images/')}}" alt="">
-                </div>
-                    <p>2023</p>
-                    <h3>Name Car</h3>
-                    <h2>Harga/<span>Day</span></h2>
-                    <a href="#" class="btn">booking</a>
-            </div>
-
-            <div class="box">
-                <div class="box-img">
-                    <img src="{{asset('images/')}}" alt="">
-                </div>
-                    <p>2023</p>
-                    <h3>Name Car</h3>
-                    <h2>Harga/<span>Day</span></h2>
-                    <a href="#" class="btn">booking</a>
-            </div>
-
-            <div class="box">
-                <div class="box-img">
-                    <img src="{{asset('images/')}}" alt="">
-                </div>
-                    <p>2023</p>
-                    <h3>Name Car</h3>
-                    <h2>Harga/<span>Day</span></h2>
-                    <a href="#" class="btn">booking</a>
-            </div>
-
-            <div class="box">
-                <div class="box-img">
-                    <img src="{{asset('images/')}}" alt="">
-                </div>
-                    <p>2023</p>
-                    <h3>Name Car</h3>
-                    <h2>Harga/<span>Day</span></h2>
-                    <a href="#" class="btn">booking</a>
-            </div>
-
-            <div class="box">
-                <div class="box-img">
-                    <img src="{{asset('images/')}}" alt="">
-                </div>
-                    <p>2023</p>
-                    <h3>Name Car</h3>
-                    <h2>Harga/<span>Day</span></h2>
-                    <a href="#" class="btn">booking</a>
-            </div>
-
-
-        </div>
-     --}}
-
+        </section>
+        @endif
     <script src="main.js"></script>
 </body>
 </html>
