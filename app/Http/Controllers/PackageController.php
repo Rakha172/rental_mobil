e56fd3cc8309ec3b489c454dc98ef581dd0b11c7
yusup <ymaolana06@gmail.com>
2023-09-12 09:40:47 +0700

    Package Controller

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Vehicle_Package;
 
 class PackageController extends Controller
 {
     public function index()
     {
-        return view('package.index');
+        $vehicle_package = Vehicle_Package::all();
+        return view('package.index',compact('vehicle_package'));
     }
 }
