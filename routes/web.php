413da756773165e5abb0c23dc3658260e601c96e
ahmadtp1 <streameren1@gmail.com>
2023-09-12 15:27:59 +0530

    commit

--- routes/web.php
+++ routes/web.php
@@ -48,13 +48,13 @@ route::middleware(['auth', 'must-admin'])->group(function(){
     Route::delete('vehicle_package/{vehicle_packages}', [Vehicle_PackageController::class, 'destroy'])->name('vehicle_package.destroy');
 });
 
-Route::middleware(['auth' , 'verified'])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    route::resource('homepage', HomepageController::class)->middleware('auth');
 });
 
+route::resource('homepage', HomepageController::class)->middleware('auth');
 
 require __DIR__.'/auth.php';
 
