8f6f9a8318a5ad7726e6c66e1a54c696f1974a95
yusup <ymaolana06@gmail.com>
2023-09-12 16:23:50 +0700

    add

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet"  type="text/css" href="{{asset('/css/vehicle.css')}}">
+    <link rel="stylesheet"  type="text/css" href="{{asset('/css/homepage.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="icon" href="{{ asset('images/jeep.png')}}">
@@ -12,7 +12,6 @@
 <body>
     <header>
         <a href="" class="logo"><img src="{{asset('images/jeep.png')}}" alt=""></a>
-
     <nav class="navbar navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{ route('homepage.index') }}">Vehicle</a>
@@ -55,22 +54,15 @@
     </nav>
     </header>
         @if($vehicle->count()>0)
-        <section class="services" id="services">
-            <div class="services-container">
+            <div class="container">
                 @foreach ( $vehicle as $vhcl )
-                <div class="box" style="width: 250px;">
-                    <div class="box-img">
+                <div class="card">
+                    <div class="card-body">
                         <img src="{{ asset($vhcl->image)}}" style="width: 230px;">
                     </div>
-
                     <a href="#" class="btn">booking</a>
                 </div>
                 @endforeach
-                @else
-                <h4 style="margin-top: 100px; text-align:center; color: grey;">There is no vehicle data, please input item data</h4>
-                <a href="{{ route('vehicle.index') }}" style="margin-left:620px; border: 1px solid grey; border-radius:5px; color:grey; padding: 5px; margin-top: 10px; position: absolute; ">Vehicle Page</a>
-            </div>
-            @endforeach
         </div>
         @endif
     <script src="main.js"></script>
