03573734d8aa6cbd281da2ee8bd9921c44a75808
yusup <ymaolana06@gmail.com>
2023-09-12 19:29:56 +0700

    add

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -54,27 +54,21 @@
         </div>
     </nav>
     </header>
-    @if(isset($vehicle))
-    <div class="text">
-        <h1>Vehicle</h1>
-    </div>
-    <div class="container">
-        @foreach ( $vehicle as $vhcl )
-        <div class="card">
-            <div class="card-body">
-                    <img src="{{ asset($vhcl->image)}}">
+        @if($vehicle->count()>0)
+        <section class="services" id="services">
+            <div class="services-container">
+                @foreach ( $vehicle as $vhcl )
+                <div class="box" style="width: 250px;">
+                    <div class="box-img">
+                        <img src="{{ asset($vhcl->image)}}" style="width: 230px;">
+                    </div>
+
+                    <a href="#" class="btn">booking</a>
                 </div>
-                <ul>
-                    <li>
-                        {{ $vhcl->brand }}
-                    </li>
-                </ul>
-                    <ul>
-                        <li>
-                            {{ $vhcl->vehicle_type }}
-                        </li>
-                    </ul>
-                <a href="#" class="btn">booking</a>
+                @endforeach
+                @else
+                <h4 style="margin-top: 100px; text-align:center; color: grey;">There is no vehicle data, please input item data</h4>
+                <a href="{{ route('vehicle.index') }}" style="margin-left:620px; border: 1px solid grey; border-radius:5px; color:grey; padding: 5px; margin-top: 10px; position: absolute; ">Vehicle Page</a>
             </div>
             @endforeach
         </div>
