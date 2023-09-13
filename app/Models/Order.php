41bdf0264647320dc3a7ace9f1404cdf1286105d
unknown <rhmakhrani@gmail.com>
2023-09-13 16:06:00 +0700

    modelorder

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,8 +10,10 @@ class Order extends Model
     protected $table = 'order';
 
     protected $fillable = [
+        'user_id',
         'rental_date',
         'return_date',
+        'vehicle_package_id',
     ];
 
     public function user()
