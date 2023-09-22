5990031bdecc1b93d2ec18858e50b9fa0250d444
yusup <ymaolana06@gmail.com>
2023-09-22 10:56:23 +0700

    ngedit tampilan info user

--- resources/views/admin/infouser.blade.php
+++ resources/views/admin/infouser.blade.php
@@ -1,74 +1,73 @@
-@extends('components.main')
+@extends('components.mainadmin')
 <x-app-layout>
-    <div class="container mt-3">
+    <div class="container-fluid">
+        <h1 class="text-center">DATA USER</h1>
         <div class="card">
             <div class="card-body">
-                <div class="shadow p-3 mb-5 bg-body rounded">
-                    <h3 class="text-center">DATA USER</h3>
                     <form action="" method="GET">
                         <div class="row mb-2">
-                            <label for="search" class="col-sm-2 col-form-label">Search</label>
                             <div class="col-sm-10">
                                 <input type="text" placeholder="Please input key for search data" name="search"
                                     autofocus style="border-radius:5px; width:80%;" value="{{ $search }}">
-                                <button class="btn btn-dark" style="height:42px;">Search</button>
+                                <button class="btn btn-light" style="height:42px;">Search</button>
                             </div>
                         </div>
                     </form>
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr class="table-dark">
-                                <th scope="col">No</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Phone Number</th>
-                                <th scope="col">Address</th>
-                                <th scope="col">Id Card Photo</th>
-                                <th scope="col">Driver Licence Photo</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @php
-                                $nomor = 1 + ($user->currentPage() - 1) * $user->perPage();
-                            @endphp
-                            @if ($user->count() > 0)
-                                @foreach ($user as $no => $usr)
-                                    <tr>
-                                        <th>{{ $nomor++ }}</th>
-                                        <td>{{ $usr->name }}</td>
-                                        <td>{{ $usr->phone_number }}</td>
-                                        <td>{{ $usr->address }}</td>
-                                        <td><img src="{{ asset($usr->id_card_photo) }}" width="100"
-                                                class="rounded mx-auto d-block"></td>
-                                        <td><img src="{{ asset($usr->driver_licence_photo) }}" width="100"
-                                                class="rounded mx-auto d-block"> </td>
-                                        <td>{{ $usr->email }}</td>
-                                        <td style="width: 150px;">
-                                            @if ($usr->role != 'admin')
-                                                <form class="" action="{{ route('register.destroy', $usr->id) }}"
-                                                    method="POST">
-                                                    @method('delete')
-                                                    @csrf
-                                                    <button type="button" class="shadow p-3 mb-5 bg-body rounded"
-                                                        data-bs-toggle="modal" data-bs-target="#exampleModal"
-                                                        name="{{ $usr->name }}"
-                                                        phone_number="{{ $usr->phone_number }}"
-                                                        address="{{ $usr->address }}"><ion-icon name="eye-outline" style="font-size: 20px; "></ion-icon></button>
-                                                    <input name="_method" type="hidden" value="DELETE">
-                                                    <button type="submit"
-                                                        class="btn btn-xs btn-flat show-alert-delete-box btn-sm  "
-                                                        data-toggle="tooltip" title='Delete'><ion-icon
-                                                            name="trash-outline"
-                                                            style="font-size: 20px; position: absolute;" class="shadow p-3 mb-5 bg-body rounded "></ion-icon ></button>
-                                                </form>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            @endif
-                        </tbody>
-                    </table>
+                    <div class="tabel-responsive">
+
+                        <table class="table table-bordered table-center">
+                            <thead>
+                                <tr class="table-border" style="background-color : rgb(236, 128, 4); color:black;">
+                                    <th scope="col">No</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Phone Number</th>
+                                    <th scope="col">Address</th>
+                                    <th scope="col">Id Card Photo</th>
+                                    <th scope="col">Driver Licence Photo</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php
+                                    $nomor = 1 + ($user->currentPage() - 1) * $user->perPage();
+                                @endphp
+                                @if ($user->count() > 0)
+                                    @foreach ($user as $no => $usr)
+                                        <tr>
+                                            <th>{{ $nomor++ }}</th>
+                                            <td>{{ $usr->name }}</td>
+                                            <td>{{ $usr->phone_number }}</td>
+                                            <td>{{ $usr->address }}</td>
+                                            <td><img src="{{ asset($usr->id_card_photo) }}" width="100"
+                                                    class="rounded mx-auto d-block"></td>
+                                            <td><img src="{{ asset($usr->driver_licence_photo) }}" width="100"
+                                                    class="rounded mx-auto d-block"> </td>
+                                            <td>{{ $usr->email }}</td>
+                                            <td style="width: 150px;">
+                                                @if ($usr->role != 'admin')
+                                                    <form class="" action="{{ route('register.destroy', $usr->id) }}"
+                                                        method="POST">
+                                                        @method('delete')
+                                                        @csrf
+                                                        <button type="button" class="btn btn-light" style="font-size: 10px;"
+                                                            data-bs-toggle="modal" data-bs-target="#exampleModal"
+                                                            name="{{ $usr->name }}"
+                                                            phone_number="{{ $usr->phone_number }}"
+                                                            address="{{ $usr->address }}">Show Account</button>
+                                                        <input name="_method" type="hidden" value="DELETE">
+                                                        <button type="submit"
+                                                            class="btn btn-light"
+                                                            data-toggle="tooltip" title='Delete'>Delate</button>
+                                                    </form>
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
                     {{-- {{ $user->links()}} --}}
                     {!! $user->appends(Request::except('page'))->render() !!}
                 </div>
