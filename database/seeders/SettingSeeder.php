a1fbfe4e7503fb7b02dfa1654ff156724b864e46
ahmadtp1 <streameren1@gmail.com>
2023-09-29 11:49:25 +0530

    commit

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Setting::create([
+            'name' => 'Novo Rental',
+            'history' => 'Novo Rental meluncurkan situs web dan aplikasi pertama mereka, memudahkan pelanggan untuk memesan kendaraan secara online. Mereka juga memperkenalkan program loyalitas yang memberikan diskon dan manfaat eksklusif bagi pelanggan setia.',
+            'image' => 'logo',
+            'images' => 'logo',
+            'location' => 'Indonesia,Jawa Barat,Bandung',
+            'no_telp' => '+62 000 000 00',
+            'email' => 'Support@gmail.com',
+            'about_me' => 'Menyediakan berbagai pilihan unit mobil mulai dari Avanza, Innova Reborn, Hiace, Alphard, Luxury Car dan berbagai pilihan lainnya',
+            'slogan' => '"Mobil Impian di Ujung Jari Anda"',
+        ]);
+    }
+}
