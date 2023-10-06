117e0b145206664ae45f690391b615f497cf41ca
unknown <rhmakhrani@gmail.com>
2023-10-06 15:17:39 +0700

    userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,9 +9,7 @@ use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
         // User::factory()->count(50)->create();
